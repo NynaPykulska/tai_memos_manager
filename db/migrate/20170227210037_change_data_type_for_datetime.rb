1cbf0ddd588d8a12531196a1f9dcdf69e3509280
Nina Pikulska <Nina Pikulska>
2017-05-23 18:06:50 +0200

    Styles

--- db/migrate/20170227210037_change_data_type_for_datetime.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDataTypeForDatetime < ActiveRecord::Migration[5.0]
-  def change
-  	change_table :memos do |t|
-      t.change :deadline, :datetime
-      t.change :time_stamp, :datetime
-    end
-  end
-end
